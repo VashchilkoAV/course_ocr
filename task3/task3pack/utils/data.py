c339c8c1350c9a723e95595e4699a294bf4cb8f8
vashchilko.av <vashchilko.av@phystech.edu>
2023-04-25 16:14:19 +0000

    task3 bboxes only

--- task3/task3pack/utils/data.py
+++ task3/task3pack/utils/data.py
@@ -4,90 +4,61 @@ from typing import Optional, Callable, List, Any, Tuple
 import numpy as np
 from PIL import Image
 import cv2
+import pandas
+import os
 
 import torch
 from torchvision import transforms
 from torchvision.datasets import VisionDataset
 from torchvision.transforms import functional as F
 
-
-class HeatmapDataset(VisionDataset):
-    def __init__(self, data_packs, split='train', transforms=None, realative=True, output_size=(128, 128)):
-        self.data_packs = data_packs
-        self.indices = []
-        self.transforms = transforms
-        self.output_size = output_size
-        self.split = split
-        self.relative = realative
-
-        for dp_idx, dp in enumerate(data_packs):
-            for im_idx, im in enumerate(dp):
-                if im.is_test_split() and split == 'test':
-                    self.indices.append((dp_idx, im_idx))
-                elif not im.is_test_split() and split == 'train':
-                    self.indices.append((dp_idx, im_idx))
-    
-
-    def __len__(self):
-        return len(self.indices)
-    
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        dp_idx, im_idx = self.indices[index]
-        image = np.array(self.data_packs[dp_idx][im_idx].image.convert('RGB'))
-        target = torch.FloatTensor(self.data_packs[dp_idx][im_idx].quadrangle)
-        
-        if not self.relative:
-            target *= self.output_size[0]
-
-        if self.transforms is not None:
-            image = self.transforms(image)
-
-        return image, target
-    
-    def get_key(self, index: int) -> str:
-        dp_idx, im_idx = self.indices[index]
-        
-        return self.data_packs[dp_idx][im_idx].unique_key
-    
-
 class SegmentationDataset(VisionDataset):
-    def __init__(self, data_packs, split='train', image_transforms=None, target_transforms=None):
-        self.data_packs = data_packs
-        self.indices = []
+    def __init__(self, images_path, csv_header, csv_encoding='utf-16', csv_path=None, image_transforms=None, target_transforms=None, is_test=False, shape=[512, 512]):
+        if not is_test:
+            self.markup = pandas.read_csv(csv_path, header=None, names=csv_header, encoding=csv_encoding)
         self.image_transforms = image_transforms
         self.target_transforms = target_transforms
-        self.split = split
-
-        for dp_idx, dp in enumerate(data_packs):
-            for im_idx, im in enumerate(dp):
-                if im.is_test_split() and split == 'test':
-                    self.indices.append((dp_idx, im_idx))
-                elif not im.is_test_split() and split == 'train':
-                    self.indices.append((dp_idx, im_idx))
+        self.images_path = images_path
+        self.is_test=is_test
+        self.shape = shape
 
     def __len__(self):
-        return len(self.indices)
+        if self.is_test:
+            return len(os.listdir(self.images_path))
+        else:
+            return len(self.markup)
 
     
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
-        dp_idx, im_idx = self.indices[index]
-        image = np.array(self.data_packs[dp_idx][im_idx].image.convert('RGB'))
+        if self.is_test:
+            image_name = os.listdir(self.images_path)[index]
+            image = Image.open(self.images_path / image_name)
+            
+        else:
+            #return torch.zeros(3, 512, 512), torch.zeros(1, 512, 512)
+            
+            image_name = self.markup.loc[index, 'path']
+            coords = self.markup.loc[index, ['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4']].to_numpy(dtype=int).reshape(4, 2)
+            if not (self.images_path / image_name).exists():
+                return torch.zeros(3, 512, 512), torch.zeros(1, 512, 512)
+            
+            image = Image.open(self.images_path / image_name)
 
-        target = np.zeros(np.array(self.data_packs[dp_idx][im_idx].image).shape[:2])
-        target = torch.FloatTensor(cv2.fillConvexPoly(target, np.array(self.data_packs[dp_idx][im_idx].gt_data['quad']), (1, )))
+            target = np.zeros((image.size[1], image.size[0]))
+            target = torch.FloatTensor(cv2.fillConvexPoly(target, coords, (1, )))
 
+            
         if self.image_transforms is not None:
             image = self.image_transforms(image)
-        if self.target_transforms is not None:
-            target = self.target_transforms(target[None, ...])
+            
+            
+        if not self.is_test:
+            if self.target_transforms is not None:
+                target = self.target_transforms(target[None, ...])
 
-        return image, target
-    
-    def get_key(self, index: int) -> str:
-        dp_idx, im_idx = self.indices[index]
+            return image, target
         
-        return self.data_packs[dp_idx][im_idx].unique_key
+        return image, image_name
     
 
 def convert_segm_to_quadr(pred, image_size):
